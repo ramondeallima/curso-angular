acb6751ce4e822394a7e7429807e3a8392bd606e
Ramon Almeida <ramondeallima@gmail.com>
2019-10-21 22:59:20 -0300

    1 - Primeiros passos 2 - Data Binding

---
+++ 01 - primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import {CursosService} from './cursos.service';
+
+
+
+@NgModule({
+  declarations: [
+    CursosComponent,
+    CursoDetalheComponent
+  ],
+  exports: [
+    CursosComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  providers: [CursosService]
+})
+export class CursosModule { }
